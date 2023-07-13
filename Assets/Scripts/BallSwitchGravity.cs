fb11ffcc71e509b9ced2d1192120c5ef1c61aea2
Murat Özdemir <101792030+Muratozdm7@users.noreply.github.com>
2023-07-13 10:12:46 +0300

    Level1 tamamlandi

--- Assets/Scripts/BallSwitchGravity.cs
+++ Assets/Scripts/BallSwitchGravity.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallSwitchGravity : MonoBehaviour
 {
@@ -18,6 +19,20 @@ public class BallSwitchGravity : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.gravityScale *= -1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("NextLevel"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
